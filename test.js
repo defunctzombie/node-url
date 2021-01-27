f392feaaece9a40c2c781486753b69399eab7cc1
Jordan Harband <ljharb@gmail.com>
2021-01-26 19:28:21 -0800

    [Tests] fix mocha tests

--- test.js
+++ test.js
@@ -19,6 +19,7 @@
 // OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
 // USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+var test = require('mocha').test;
 var assert = require('assert');
 
 var url = require('./url');
